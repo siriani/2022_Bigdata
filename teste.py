3d6d3036a500547e54220ecbb36a324f0b3c0b97
siriani <allan@fatecpomepeia.edu.br>
2022-02-25 16:17:23 -0300

    Adicionado funções

--- teste.py
+++ teste.py
@@ -1,7 +1,15 @@
-print("soma números")
-print("Digite o primeiro número:")
-a = int(input())
+def soma(a,b):
+    return a+b
+
+def subtracao(a,b):
+    return a-b
+
+#print("Digite o primeiro número:")
+a = int(input("Digite o primeiro numero:\n"))
+print("Digite o segundo número:")
 b = int(input())
-soma = a + b
-print(soma)
+
+print("Está é a soma = ",soma(a,b))
+print("Está é a subtracao = ",subtracao(a,b))
+
 
